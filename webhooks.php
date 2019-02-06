b75c47c728cb8708aeef8a87ac94a82b42bb11ed
erkawit <erkawit.2099@gmail.com>
2019-02-06 20:17:10 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'LyH2DdaIjCX65BjEaMynO8iK6zAAd02bqmiXuxm6NBBgH86TVqTuBsdpMtsmqzHQpeSGe5mRBKUKcScdIkv0DvjYw0Asz1FWAskFemoXpwT0NJzfyi1KeaFNuny85+6Hya9OhM2vq/lDiagYurStewdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
